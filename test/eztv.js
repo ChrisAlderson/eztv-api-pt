5c4da5d257f6b26c4e2751c2e2687e7ee1415ab7
Chris Alderson <chrisalderson@protonmail.com>
2017-07-17 20:34:46 +0000

    more tests for module

--- test/eztv.js
+++ test/eztv.js
@@ -1,66 +1,116 @@
 'use strict'
 
-const { assert } = require('chai')
+const { expect } = require('chai')
 const EZTV = require('../eztv-api-pt')
 
 describe('EZTV', () => {
 
-  let eztv, data, falseData
-  before(() => {
-    eztv = new EZTV()
+  let eztv, show, falseShow, dateBasedShow, noEpisodesShow, noMagnetShow
 
-    data = {
+  before(() => {
+    console.warn = () => {}
+    eztv = new EZTV({
+      debug: true
+    })
+
+    show = {
       show: '10 O\'Clock Live',
-      id: '449',
+      id: 449,
       slug: '10-o-clock-live'
     }
-
-    falseData = {
+    falseShow = {
       show: 'False Show Name',
-      id: '12345',
+      id: 12345,
       slug: 'false-show-name'
+    }
+    dateBasedShow = {
+      show: '60 Minutes US',
+      id: 817,
+      slug: '60-minutes-us'
+    }
+    noEpisodesShow = {
+      show: '2010 Vancouver Winter Olympics',
+      id: 350,
+      slug: '2010-vancouver-winter-olympics'
+    }
+    noMagnetShow = {
+      show: 'Grimm',
+      id: 556,
+      slug: 'grimm'
     }
   })
 
-  it('Should get a list of tv shows', done => {
+  function testShowAttributes(show) {
+    expect(show).to.be.an('object')
+    expect(show.show).to.be.a('string')
+    expect(show.id).to.be.a('number')
+    expect(show.slug).to.be.a('string')
+  }
+
+  it('should get a list of tv shows', done => {
     eztv.getAllShows().then(res => {
-      assert.isArray(res)
+      const random = Math.floor(Math.random() * res.length)
+      testShowAttributes(res[random])
+
       done()
-    }).catch(err => done(err))
+    }).catch(done)
   })
 
-  it('Should get episodes of a tv show', done => {
-    eztv.getShowData(data).then(res => {
-      assert.isObject(res)
+  it('should get a show', done => {
+    eztv.getShowData(show).then(res => {
+      testShowAttributes(res)
+      expect(res.imdb).to.be.a('string')
+      expect(res.episodes).to.be.an('object')
+
       done()
-    }).catch(err => done(err))
+    }).catch(done)
   })
 
-  it('Should get all the episodes of a tv show', done => {
-    eztv.getShowEpisodes(data).then(res => {
-      assert.isObject(res)
+  it('should search for a show', done => {
+    eztv.getShowEpisodes(show).then(res => {
+      testShowAttributes(res)
+      expect(res.episodes).to.be.an('object')
+
       done()
-    }).catch(err => done(err))
+    }).catch(done)
   })
 
-  it('Should get fail to get episodes of a tv show', done => {
-    eztv.getShowData(falseData).then(res => {
-      assert.isObject(res)
+  it('should get a date based show', done => {
+    eztv.getShowData(dateBasedShow).then(res => {
+      testShowAttributes(res)
+      expect(res.imdb).to.be.a('string')
+      expect(res.episodes).to.be.an('object')
+
       done()
-    }).catch(err => {
-      assert.isOk(err)
-      done()
-    })
+    }).catch(done)
   })
 
-  it('Should get fail to get all the episodes of a tv show', done => {
-    eztv.getShowEpisodes(falseData).then(res => {
-      assert.isObject(res)
+  it('should get a show with no episodes', done => {
+    eztv.getShowData(noEpisodesShow).then(res => {
+      testShowAttributes(res)
+      expect(res.imdb).to.be.a('string')
+
       done()
-    }).catch(err => {
-      assert.isOk(err)
+    }).catch(done)
+  })
+
+  it('should get a show with no magnet links', done => {
+    eztv.getShowData(noMagnetShow).then(res => {
+      testShowAttributes(res)
+      expect(res.imdb).to.be.a('string')
+      expect(res.episodes).to.be.an('object')
+      
       done()
-    })
+    }).catch(done)
+  })
+
+  it('should fail to get a show', done => {
+    eztv.getShowData(falseShow)
+      .then(done)
+      .catch(err => {
+        expect(err).to.be.an('Error')
+        done()
+      })
   })
 
 })
